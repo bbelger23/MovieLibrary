07b0ad58cd99ea080e3d4c9fc8a80555c69c7786
Brandon Belger <bbelger@wctc.edu>
2022-02-19 14:26:57 -0600

    user can add to list

--- Program.cs
+++ Program.cs
@@ -28,34 +28,36 @@ namespace MovieLibrary
                 List<string> MovieGenre = new List<string>();
 
                 StreamReader sr = new StreamReader(file);
+                // read first line due to headers
                 sr.ReadLine();
                 while (!sr.EndOfStream)
                 {
                     string row = sr.ReadLine();
-
+                    // looking for quote in string
                     int idx = row.IndexOf('"');
                     if (idx == -1)
                     {
+                        // movie data is separated by comma
                         string[] movie = row.Split(",");
                         
                         MovieID.Add(UInt64.Parse(movie[0]));
 
                         MovieTitle.Add(movie[1]);
-
+                        // replacing "|" with ", "
                         MovieGenre.Add(movie[2].Replace("|", ", "));
                     }
                     else
                     {
                         MovieID.Add(UInt64.Parse(row.Substring(0, idx - 1)));
-                           
+                        // removed ID and first quote from string   
                         row = row.Substring(idx + 1);
-                            
+                        // finds the next quote    
                         idx = row.IndexOf('"');
                             
                         MovieTitle.Add(row.Substring(0, idx));
-                            
+                        // removes title and last comma from string    
                         row = row.Substring(idx + 2);
-                            
+                        // replacing "|" with ", "    
                         MovieGenre.Add(row.Replace("|", ", "));
                     }
                 }
@@ -70,8 +72,10 @@ namespace MovieLibrary
 
                 if (option == "1")
                 {
+                    // loops through file
                     for (int i = 0; i < MovieID.Count; i++)
                     {
+                        // displays all of the entries in file
                         Console.WriteLine($"MovieID: {MovieID[i]}");
                         Console.WriteLine($"Title: {MovieTitle[i]}");
                         Console.WriteLine($"Genre: {MovieGenre[i]}");
@@ -80,7 +84,49 @@ namespace MovieLibrary
                 }
                 else if (option == "2")
                 {
-                
+                    // ask user for movie title
+                    Console.WriteLine("Enter the movie title");
+
+                    string title = Console.ReadLine();
+
+                    List<string> LowerCaseMovieTitle = MovieTitle.ConvertAll(t => t.ToLower());
+                    if (LowerCaseMovieTitle.Contains(title.ToLower()))
+                    {
+                        logger.Info("Duplicate movie title {title} found", title);
+                    }
+                    else
+                    {
+                        UInt64 Id = MovieID.Max() + 1;
+                        List<string> moviegenre = new List<string>();
+                        string genre;
+                        do{
+                            Console.WriteLine("Enter a genre");
+
+                            genre = Console.ReadLine();
+
+                            if (genre != "" && genre.Length > 0)
+                            {
+                                moviegenre.Add(genre);
+                            }
+                        } while (genre != "");
+
+                        if (moviegenre.Count == 0)
+                        {
+                            moviegenre.Add("(no genre)");
+                        }
+
+                        string genreString = string.Join("|", moviegenre);
+
+                        title = title.IndexOf(',') != -1 ? $"\"{title}\"" : title; 
+
+                        StreamWriter sw = new StreamWriter(file);
+                        sw.WriteLine($"{Id},{title},{genreString}");
+                        sw.Close();
+
+                        MovieID.Add(Id);
+                        MovieTitle.Add(title);
+                        MovieGenre.Add(genreString);
+                    }
                 }
             }
         }
