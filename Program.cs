8a5204a3490116c36c61116541256d072c79fac9
Brandon Belger <bbelger@wctc.edu>
2022-02-19 13:47:32 -0600

    modified to list

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.IO;
+using System.Collections.Generic;
+using System.Linq;
 using NLog.Web;
 
 namespace MovieLibrary
@@ -12,41 +14,74 @@ namespace MovieLibrary
             
             var logger = NLog.Web.NLogBuilder.ConfigureNLog(path).GetCurrentClassLogger();
             
-            var file = "movies.csv";
+            string file = "movies.csv";
 
-            Console.WriteLine("Enter 1 to see movies");
-            Console.WriteLine("Enter 2 to add movie");
-            Console.WriteLine("Press any other key to exit");
-
-            string option = Console.ReadLine();
-
-            if (option == "1")
+            if (!File.Exists(file))
             {
-                if (File.Exists(file))
-                {
-                    StreamReader sr = new StreamReader(file);
-                    while (!sr.EndOfStream)
-                    {
-                        string row = sr.ReadLine();
+                logger.Warn("File does not exist. {file}", file);
+            }
+            else
+            {
 
+                List<UInt64> MovieID = new List<UInt64>();
+                List<string> MovieTitle = new List<string>();
+                List<string> MovieGenre = new List<string>();
+
+                StreamReader sr = new StreamReader(file);
+                sr.ReadLine();
+                while (!sr.EndOfStream)
+                {
+                    string row = sr.ReadLine();
+
+                    int idx = row.IndexOf('"');
+                    if (idx == -1)
+                    {
                         string[] movie = row.Split(",");
                         
-                        string movieID = movie[0];
+                        MovieID.Add(UInt64.Parse(movie[0]));
 
+                        MovieTitle.Add(movie[1]);
 
-                        string genre = movie[2].Replace("|", ",");
-
-                        Console.WriteLine($"MovieID: {movieID}, Genre: {genre}");
+                        MovieGenre.Add(movie[2].Replace("|", ", "));
+                    }
+                    else
+                    {
+                        MovieID.Add(UInt64.Parse(row.Substring(0, idx - 1)));
+                           
+                        row = row.Substring(idx + 1);
+                            
+                        idx = row.IndexOf('"');
+                            
+                        MovieTitle.Add(row.Substring(0, idx));
+                            
+                        row = row.Substring(idx + 2);
+                            
+                        MovieGenre.Add(row.Replace("|", ", "));
                     }
                 }
-                else
+                sr.Close();
+            
+
+                Console.WriteLine("Enter 1 to see movies");
+                Console.WriteLine("Enter 2 to add movie");
+                Console.WriteLine("Press any other key to exit");
+
+                string option = Console.ReadLine();
+
+                if (option == "1")
                 {
-                    logger.Warn("File does not exist. {file}", file);
+                    for (int i = 0; i < MovieID.Count; i++)
+                    {
+                        Console.WriteLine($"MovieID: {MovieID[i]}");
+                        Console.WriteLine($"Title: {MovieTitle[i]}");
+                        Console.WriteLine($"Genre: {MovieGenre[i]}");
+                        Console.WriteLine();
+                    }
                 }
-            }
-            else if (option == "2")
-            {
+                else if (option == "2")
+                {
                 
+                }
             }
         }
     }
